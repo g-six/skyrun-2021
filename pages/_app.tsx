787e553298d2b353eaff8893ff05608e39d4c915
G6 <g6@mini.local>
2021-08-21 09:36:29 +0800

    refactor: 💡 remove NextAuth provider as this won't work on S5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import type { AppProps } from 'next/app'
-import { Provider } from 'next-auth/client'
 import '../styles/globals.scss'
 import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Provider session={pageProps.session}>
+        <>
             <Component {...pageProps} />
             <Footer />
-        </Provider>
+        </>
     )
 }
 export default MyApp

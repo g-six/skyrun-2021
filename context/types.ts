a032c30a6be45a668618ff27ee0073a04b71dab6
G6 <g6@mini.local>
2021-09-10 17:29:05 +0800

    feat: 🎸 forgot password

--- context/types.ts
+++ context/types.ts
@@ -1,4 +1,4 @@
-import { AuthenticationResultType } from '@aws-sdk/client-cognito-identity-provider'
+import { AuthenticationResultType, ConfirmForgotPasswordCommandOutput } from '@aws-sdk/client-cognito-identity-provider'
 import { ModalHook } from 'components/Modals/types'
 import { UserModel } from 'services/profile'
 
@@ -18,6 +18,8 @@ export type AuthContextType = {
     user?: SkyUser
     LoginModal: ModalHook
     SignupModal: ModalHook
+    confirmForgotPassword(email: string, new_password: string, code: string): Promise<ConfirmForgotPasswordCommandOutput>
+    forgotPassword(email: string): Promise<void>
     login(email: string, password: string): Promise<AuthenticationResultType | boolean>
     signup(email: string, password: string, first_name: string, last_name: string): Promise<string | boolean>
     logout(): Promise<void>

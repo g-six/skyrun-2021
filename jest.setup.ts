d377684809820b6f1afc52dc827c013618f09845
G6 <g6@mini.local>
2021-09-13 07:02:58 +0800

    test: 💍 update jest config

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,6 @@
 import { configure } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+const mockEnvConfig = {}
 
+jest.mock('next/config', () => () => ({ publicRuntimeConfig: mockEnvConfig }))
 configure({ adapter: new Adapter() })

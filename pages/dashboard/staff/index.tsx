9238cd1dd5cc976604980126b396e4296d59afee
G6 <g6@mini.local>
2021-10-11 16:19:41 +0800

    feat: 🎸 Staff table / dashboard

--- pages/dashboard/staff/index.tsx
+++ pages/dashboard/staff/index.tsx
@@ -1,9 +1,217 @@
+import CreateStaffModal, {
+    CreateStaffModalOpener,
+} from 'components/Modals/Staff'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { classNames } from 'utils/dom-helpers'
 import Dashboard from '..'
+import { FetchMethods, useFetch } from 'utils/fetch-helper'
+import { useAuth } from 'context/AuthContext'
+
+function SearchInputGroup({ selected_idx = 0 }) {
+    return (
+        <div className="relative rounded-md shadow-sm mx-4 divide-x divide-gray-200">
+            <div className="absolute inset-y-0 left-0 pl-5 flex items-center pointer-events-none">
+                <span className="feather feather-search text-gray-500 text-lg" />
+            </div>
+            <input
+                type="text"
+                name="keyword"
+                id="keyword"
+                className={classNames(
+                    'focus:ring-primary-dark focus:border-primary-dark rounded-md',
+                    'block w-full py-2 pl-12 pr-28',
+                    'border-gray-300 text-gray-500 placeholder-gray-300'
+                )}
+                placeholder="Search..."
+            />
+            <div className="absolute inset-y-0 right-0 flex items-center">
+                <label htmlFor="Category" className="sr-only">
+                    Category
+                </label>
+                <select
+                    id="category"
+                    name="category"
+                    className={classNames(
+                        'h-full py-0 pl-2 pr-8 bg-transparent',
+                        'focus:ring-primary-dark focus:border-primary-dark ',
+                        'border-transparent rounded-r-md',
+                        selected_idx > 0 ? 'text-gray-500' : 'text-gray-300'
+                    )}
+                    defaultValue="Categpry"
+                >
+                    <option disabled>Category</option>
+                    <option>Location</option>
+                    <option>Package</option>
+                </select>
+            </div>
+        </div>
+    )
+}
+
+type HeaderProps = {
+    onSearch: Dispatch<SetStateAction<string>>
+}
+
+type StaffItem = {
+    id: string
+    user: Record<string, string>
+}
+type StaffListResponse = {
+    [key: string]:
+        | string
+        | boolean
+        | number
+        | Record<string, string>
+        | StaffItem[]
+}
+
+function HeaderActions(props: HeaderProps) {
+    return (
+        <>
+            <SearchInputGroup />
+            <CreateStaffModalOpener className="bg-primary text-white px-8 py-2 text-lg font-light rounded-lg" />
+        </>
+    )
+}
+
+function ListHeader() {
+    return (
+        <thead className="bg-gray-50">
+            <tr>
+                <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                    Name
+                </th>
+                <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                    Title
+                </th>
+                <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                    Status
+                </th>
+                <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                    Role
+                </th>
+                <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Edit</span>
+                </th>
+            </tr>
+        </thead>
+    )
+}
 
 function DashboardStaff() {
+    const ctx = useAuth()
+    const [selected_search_category, setSearchCategory] = useState('')
+    const [staff, setStaff] = useState()
+    const { is_loading, data, status, doFetch } = useFetch(
+        `/v1/staff/?tenantId=${ctx.tenant?.id}`,
+        FetchMethods.GET,
+        false
+    )
+
+    useEffect(() => {
+        async function fetchData() {
+            await doFetch()
+            setStaff(data.content)
+        }
+        if (ctx.tenant?.id && !staff) {
+            fetchData()
+        }
+    }, [staff, ctx.tenant?.id, doFetch, data, is_loading])
+
     return (
-        <Dashboard>
-            <span>Staff panel is a work in progress</span>
+        <Dashboard actions={<HeaderActions onSearch={setSearchCategory} />}>
+            <div className="flex flex-col">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Name
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Email
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Phone
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Status
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="relative px-6 py-3"
+                                        >
+                                            <span className="sr-only">
+                                                Edit
+                                            </span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {(staff || []).map((record: StaffItem) => (
+                                        <tr key={record.user.email}>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="text-sm font-medium text-gray-900">
+                                                    {[record.user.firstName, record.user.lastName].join(' ')}
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="text-sm text-gray-900">
+                                                    {record.user.email}
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {record.user.phone || 'None Specified'}
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800 uppercase">
+                                                    Current member
+                                                </span>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a
+                                                    href="#"
+                                                    className="text-indigo-600 hover:text-indigo-900"
+                                                >
+                                                    Edit
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <CreateStaffModal />
         </Dashboard>
     )
 }

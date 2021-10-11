69c4b31833474f3436ff54fb346b6d2a37b8638d
G6 <g6@mini.local>
2021-10-12 05:35:43 +0800

    feat: 🎸 location modal context

--- context/types.ts
+++ context/types.ts
@@ -40,6 +40,7 @@ export type AuthContextType = {
     LoginModal: ModalHook
     SignupModal: ModalHook
     CreateClientModal: ModalHook
+    CreateLocationModal: ModalHook
     CreateStaffModal: ModalHook
     confirmForgotPassword(email: string, new_password: string, code: string): Promise<ConfirmForgotPasswordCommandOutput>
     forgotPassword(email: string): Promise<void>

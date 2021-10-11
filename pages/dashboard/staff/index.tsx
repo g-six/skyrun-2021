fc140154ba638f080b68e171679e90f5b6a4a10a
G6 <g6@mini.local>
2021-10-11 22:41:23 +0800

    feat: 🎸 staff card

--- pages/dashboard/staff/index.tsx
+++ pages/dashboard/staff/index.tsx
@@ -132,83 +132,27 @@ function DashboardStaff() {
 
     return (
         <Dashboard actions={<HeaderActions onSearch={setSearchCategory} />}>
-            <div className="flex flex-col">
-                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table className="min-w-full divide-y divide-gray-200">
-                                <thead className="bg-gray-50">
-                                    <tr>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                            Name
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                            Email
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                            Phone
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                            Status
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="relative px-6 py-3"
-                                        >
-                                            <span className="sr-only">
-                                                Edit
-                                            </span>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    {(staff || []).map((record: StaffItem) => (
-                                        <tr key={record.user.email}>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm font-medium text-gray-900">
-                                                    {[record.user.firstName, record.user.lastName].join(' ')}
-                                                </div>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-900">
-                                                    {record.user.email}
-                                                </div>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                                {record.user.phone || 'None Specified'}
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800 uppercase">
-                                                    Current member
-                                                </span>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <a
-                                                    href="#"
-                                                    className="text-indigo-600 hover:text-indigo-900"
-                                                >
-                                                    Edit
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-8">
+                {(staff || []).map((record: StaffItem) => (
+                    <div key={record.user.email} className="shadow-2xl p-8 rounded-xl border-t border-l border-gray-50 h-96 text-center">
+                        <div className="text-sm font-medium text-gray-900">
+                            {[record.user.firstName, record.user.lastName].join(' ')}
                         </div>
+                        <div className="text-sm text-gray-900">
+                            {record.user.email}
+                        </div>
+                        {record.user.phone || 'None Specified'}
+                        <div className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800 uppercase">
+                            Current member
+                        </div>
+                        <a
+                            href="#"
+                            className="text-indigo-600 hover:text-indigo-900"
+                        >
+                            Edit
+                        </a>
                     </div>
-                </div>
+                ))}
             </div>
 
             <CreateStaffModal />

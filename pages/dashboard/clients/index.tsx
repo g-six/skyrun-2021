c465541500648f3437c1cd7b8dde538f77011f45
G6 <g6@mini.local>
2021-10-19 18:05:28 +0800

    feat: 🎸 move create button to drawer content

--- pages/dashboard/clients/index.tsx
+++ pages/dashboard/clients/index.tsx
@@ -61,7 +61,6 @@ function HeaderActions(props: HeaderProps) {
     return (
         <>
             <SearchInputGroup />
-            <CreateClientModalOpener className="bg-primary text-white px-8 py-2 text-lg font-light rounded-lg" />
         </>
     )
 }
@@ -91,6 +90,7 @@ function DashboardClient() {
             <div className="flex flex-col">
                 <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <CreateClientModalOpener className="bg-primary text-white px-8 py-2 text-lg font-light rounded-lg" />
                         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                             <table className="min-w-full divide-y divide-gray-200">
                                 <thead className="bg-gray-50">

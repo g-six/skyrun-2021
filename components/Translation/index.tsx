deb67616b69adf0cce171b3e62bb9ab5209293cf
G6 <g6@mini.local>
2021-10-28 17:37:08 +0800

    style: 💄 prettier formatted component

--- components/Translation/index.tsx
+++ components/Translation/index.tsx
@@ -6,22 +6,26 @@ export type TranslationProps = {
 }
 export function Translation(p: TranslationProps) {
     if (p.render_as == 'div') {
-        return <div className={p.className || ''}>
-            {p.translations[p.content_key] || p.content_key}
-        </div>
+        return (
+            <div className={p.className || ''}>
+                {p.translations[p.content_key] || p.content_key}
+            </div>
+        )
     } else if (p.render_as == 'p') {
-        return <p className={p.className || ''}>
-            {p.translations[p.content_key] || p.content_key}
-        </p>
+        return (
+            <p className={p.className || ''}>
+                {p.translations[p.content_key] || p.content_key}
+            </p>
+        )
     } else if (p.render_as == 'span') {
-        return <span className={p.className || ''}>
-            {p.translations[p.content_key] || p.content_key}
-        </span>
+        return (
+            <span className={p.className || ''}>
+                {p.translations[p.content_key] || p.content_key}
+            </span>
+        )
     }
 
-    return <>
-        {p.translations[p.content_key] || p.content_key}
-    </>
+    return <>{p.translations[p.content_key] || p.content_key}</>
 }
 
 export default Translation

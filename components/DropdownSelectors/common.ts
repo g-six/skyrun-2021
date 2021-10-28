98a21b5c18fbfdeb16381cb1ab5dbce8433d92a8
G6 <g6@mini.local>
2021-10-28 15:44:31 +0800

    fix: 🐛 wonky client form validation

--- components/DropdownSelectors/common.ts
+++ components/DropdownSelectors/common.ts
@@ -1,15 +1,17 @@
-import { Dispatch, ReactNode, SetStateAction } from 'react'
+import { Dispatch, ReactElement, ReactNode, SetStateAction } from 'react'
 
 export enum DropPosition {
     TOP_LEFT = 'origin-top-left left-0',
     TOP_RIGHT = 'origin-top-right right-0',
 }
 export type SelectorItemProps = {
-    label: string
+    label?: ReactElement
     updateGroupSelection?(selection: number[]): void
     is_expanded?: boolean
     onClick?: (expanded: boolean) => void
-    selection: number[]
+    value?: string
+    selection?: number[]
+    input_component?: ReactElement
     options?: {
         label: string
         value: string
@@ -18,8 +20,11 @@ export type SelectorItemProps = {
 
 export type SelectorProps = {
     className?: string
+    dropboxClassname?: string
     position?: DropPosition
     items: SelectorItemProps[]
     selected?: Record<string, string>
     is_search_enabled?: boolean
+    label?: ReactElement
+    style?: Record<string, string>
 }

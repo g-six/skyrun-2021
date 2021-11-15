2b2047038eef2eadb28e55c805b5702545f402c4
Jefferson Bacareza <jefferson@nerubia.com>
2021-11-15 10:25:14 +0800

    Formatting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,21 +2,20 @@ import { FB_PIXEL_ID } from 'lib/fbpixels'
 import { GA_TRACKING_ID } from 'lib/gtag'
 import { Head, Html, Main, NextScript } from 'next/document'
 
-
 function Document() {
-  return (
-    <Html>
-      <Head>
-        {/* enable analytics script only for production */}
-        <>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
+    return (
+        <Html>
+            <Head>
+                {/* enable analytics script only for production */}
+                <>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                        // eslint-disable-next-line react/no-danger
+                        dangerouslySetInnerHTML={{
+                            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -24,26 +23,26 @@ function Document() {
               page_path: window.location.pathname,
             });
           `,
-            }}
-          />
+                        }}
+                    />
 
-          <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: 'none' }}
-              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-              alt="no-script"
-            />
-          </noscript>
-        </>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+                    <noscript>
+                        <img
+                            height="1"
+                            width="1"
+                            style={{ display: 'none' }}
+                            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                            alt="no-script"
+                        />
+                    </noscript>
+                </>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
 
 export default Document

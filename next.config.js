59a0a33e36e0957f1f0ac9082590a70ec330c936
Jefferson Bacareza <jefferson@nerubia.com>
2021-11-18 11:13:32 +0800

    Update logging

--- next.config.js
+++ next.config.js
@@ -41,11 +41,17 @@ module.exports = {
       '/': { page: '/' },
       ...translated_pages,
     })
-    console.log('BITBUCKET_BRANCH:', BITBUCKET_BRANCH)
-    console.log('NODE_ENV:', NODE_ENV)
-    console.log('GA_MEASUREMENT_ID_PROD:', GA_MEASUREMENT_ID_PROD)
-    console.log('GA_MEASUREMENT_ID_PROD:', GA_MEASUREMENT_ID_PROD)
-    console.log('FACEBOOK_PIXEL_ID:', FACEBOOK_PIXEL_ID)
+    console.log('BITBUCKET_BRANCH:', process.env.BITBUCKET_BRANCH)
+    console.log('NODE_ENV:', process.env.NODE_ENV)
+    console.log(
+        'GA_MEASUREMENT_ID_PROD:',
+        process.env.GA_MEASUREMENT_ID_PROD
+    )
+    console.log(
+        'GA_MEASUREMENT_ID_PROD:',
+        process.env.GA_MEASUREMENT_ID_PROD
+    )
+    console.log('FACEBOOK_PIXEL_ID:', process.env.FACEBOOK_PIXEL_ID)
     return {
       '/': { page: process.env.LANDING_PAGE || '/' },
       ...translated_pages,

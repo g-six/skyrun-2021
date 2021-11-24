52f4cc0b151be2fca1599acd9b285b864cf12e36
Jefferson Bacareza <jefferson@nerubia.com>
2021-11-24 12:15:26 +0800

    Remove page path on GA config

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -6,9 +6,7 @@ export const GA_TRACKING_ID = BITBUCKET_BRANCH != "prod" ? GA_MEASUREMENT_ID_NON
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  })
+  window.gtag("config", GA_TRACKING_ID)
 }
 
 type GTagEvent = {
